45ff0f29f8bd58c1813c6bf4a944bafb65b03345
anassuv <114801727+anassuv@users.noreply.github.com>
2022-11-06 09:19:23 +0300

    z13

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Z
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 100)
+            {
+                Console.WriteLine("третьей цифры нет");
+            }
+            else
+            {
+                while (n > 1000)
+                {
+                    n /= 10;
+                }
+                Console.WriteLine("{0}", n % 10);
+            }
+            Console.ReadKey();
+        }
+    }
+}
